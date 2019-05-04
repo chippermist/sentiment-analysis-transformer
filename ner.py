a27a9c0361b32a43154369d7690eb7ec5b7dd53a
Chinmay Garg <chinmaygarg@me.com>
2019-05-03 17:30:39 -0700

    added test for named entity recogn.

--- ner.py
+++ ner.py
@@ -1,16 +1,9 @@
 import nltk
 from data import load_training_data
 
-if __name__ == '__main__':
-  raw_data = load_training_data()
-
-
+def name_entity_recognition(raw_data):
   for x in raw_data['review'] :
     tokens = nltk.word_tokenize(x)
     tagged = nltk.pos_tag(tokens)
     entities = nltk.chunk.ne_chunk(tagged)
-    print()
-    print() 
-    print(entities)
-    print() 
-    print() 
+    return entities
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from data import load_testing_data, load_training_data
+from ner import name_entity_recognition
 
 if __name__ == '__main__':
   raw_data = load_training_data()
   print(raw_data.keys())
+  print()
+
+  # named entity recognition using nltk
+  entities = name_entity_recognition(raw_data)
+  print(entities[10:])
