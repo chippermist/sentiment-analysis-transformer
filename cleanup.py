334501516b1bad4adff1f42c1b6d8ce71b7841fd
Chinmay Garg <chinmaygarg@me.com>
2019-05-07 09:42:55 -0700

    added better cleanup
    - Converted all reviews to lowercase
    - Removed all puncuations
    - Changed Rating to positive, neutral or negative
    - Generated new files using the new cleanup

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,13 @@
 import csv
+from string import punctuation
 
+
+# Generates the cleaner data after removing unnecessary characters
 def generate_clean_csv(filename, newfilename):
   inputfile = csv.reader(open(filename,'r'))
 
+  # replace and overwrite if exists
+  # create if doesn't -- writing new clean data
   with open(newfilename, mode='w') as clean_file:
     outputfile = csv.writer(clean_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
@@ -12,25 +17,35 @@ def generate_clean_csv(filename, newfilename):
       # writing the first row as-is
       if i == 0:
         outputfile.writerow(row)
-        i += 1
+        i  += 1
         continue
 
+      # converting sentence to all lowercase
+      row[3]   = row[3].lower()
       # replacing characters in the review
-      row[3] = row[3].replace('&#039;', "'")
-      row[3] = row[3].replace('&amp;', '&')
+      row[3]   = row[3].replace('&#039;', "'")
+      row[3]   = row[3].replace('&amp;', '&')
+      row[3]   = ''.join([c for c in row[3] if c not in punctuation])
+
+      # changing rating to positive, neutral and negative
+      # since the numbers will be pretty pointless overall
+      if row[4] in ['1', '2', '3']:
+        row[4] = 'negative'
+      elif row[4] in ['4','5','6']:
+        row[4] = 'neutral'
+      else:
+        row[4] = 'positive'
       outputfile.writerow(row)
 
-# uncomment to use 
-# make sure the cleaned data does not already exist in dataset/
-
 if __name__ == '__main__':
-  print 'You are about to create new data files.\n'
-  if raw_input('Are you sure you want to run cleanup on data? ') == 'y':
+  print('You are about to create new data files.\n')
+  if input('Are you sure you want to run cleanup on data? (y/n) ') == 'y':
     # generate clean csv for training data
     generate_clean_csv('dataset/drug-data/drugsTrain_raw.csv', 'dataset/drug-data/drugsTrain_raw_clean.csv')
+    print('Clean training data -- created.\n')
     # generate clean csv for testing data
     generate_clean_csv('dataset/drug-data/drugsTest_raw.csv', 'dataset/drug-data/drugsTest_raw_clean.csv')
-  
+    print('Clean testing data -- created.')
 
 
 
