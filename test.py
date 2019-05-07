d21de6a6b96a585f8ba4ef2a4854225d94a0a059
Chinmay Garg <chinmaygarg@me.com>
2019-05-07 09:47:21 -0700

    added check for file

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from data import load_testing_data, load_training_data
 from ner import name_entity_recognition
+import os
 
 if __name__ == '__main__':
+
+  # check if file exists
+  exists = os.path.isfile('dataset/drug-data/drugsTrain_raw_clean.csv')
+  if not exists:
+    print('File does not exist. \nPlease run `cleanup.py` to create files.')
+
   raw_data = load_training_data()
   print(raw_data.keys())
   print()
@@ -9,3 +16,6 @@ if __name__ == '__main__':
   # named entity recognition using nltk
   entities = name_entity_recognition(raw_data)
   print(entities[10:])
+
+
+
