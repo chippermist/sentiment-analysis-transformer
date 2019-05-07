5fefcaaa077a15cbfe32f1df21417c76ed591e11
Chinmay Garg <chinmaygarg@me.com>
2019-05-07 15:00:45 -0700

    added data_analysis.py and changed tokenize due to name conflict with nltk
    Added data_analysis that will show you tokenized data analysis with length. Can also be used to print words and frequencies.
    
    tokenize_data performs the tokenization now. File has been renamed due to conflict with something within `nltk` module.

--- ner.py
+++ ner.py
@@ -3,7 +3,7 @@ from data import load_training_data
 
 def name_entity_recognition(raw_data):
   for x in raw_data['review'] :
-    tokens = nltk.word_tokenize(x)
-    tagged = nltk.pos_tag(tokens)
+    tokens   = nltk.word_tokenize(x)
+    tagged   = nltk.pos_tag(tokens)
     entities = nltk.chunk.ne_chunk(tagged)
     return entities
---
+++ tokenize_data.py
@@ -0,0 +1,30 @@
+from collections import Counter
+import numpy as np
+
+def count_total_words(data):
+  all_text     = ' '.join(data)
+  words        = all_text.split()
+  # count all the words using Counter Method
+  count_words  = Counter(words)
+  total_words  = len(words)
+  sorted_words = count_words.most_common(total_words)
+  return sorted_words
+
+def vocab_to_int_mapping(data):
+  sorted_words = count_total_words(data)
+  vocab_to_int = {w:i+1 for i, (w,c) in enumerate(sorted_words)}
+  # print(vocab_to_int)
+  return vocab_to_int
+
+def encode_the_words(data):
+  vocab_to_int = vocab_to_int_mapping(data)
+  reviews_int  = []
+  for review in data:
+      r = [vocab_to_int[w] for w in review.split()]
+      reviews_int.append(r)
+  # print (reviews_int[0:3])
+  return reviews_int
+
+def encode_the_labels(labels):
+  encoded_labels = [1 if label =='positive' else -1 if label =='negative' else 0 in labels_split]
+  encoded_labels = np.array(encoded_labels)
